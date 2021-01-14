624a7f3e0d42d9d7a0bdb6c5c8ce13d64e256a15
TiLongZS <tilongzs@qq.com>
2021-01-14 20:55:58 +0800

    增加UDP测试工程

---
+++ socket_udp_test/android/app/src/main/java/moe/mengmei/flutter/socket_udp_test/MainActivity.java
@@ -0,0 +1,6 @@
+package moe.mengmei.flutter.socket_udp_test;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ socket_udp_test/lib/main.dart
@@ -0,0 +1,403 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:typed_data';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:wifi_ip/wifi_ip.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'App',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomePage(),
+      },
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _HomePageState();
+  }
+}
+
+// 自定义滚动条
+class _ScrollBar extends StatelessWidget {
+  double _viewHeight = 1;
+  double _parentHeight = 1;
+
+  _ScrollBar(double viewHeight, double parentHeight) {
+    _viewHeight = viewHeight;
+    _parentHeight = parentHeight;
+  }
+
+  double GenerateHeight() {
+    if (_viewHeight == 0) {
+      return 0;
+    } else {
+      double height = _parentHeight * _parentHeight / _viewHeight;
+      return height < 50 ? 50 : height;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 18,
+      height: GenerateHeight(),
+      decoration: BoxDecoration(
+          shape: BoxShape.rectangle,
+          borderRadius: BorderRadius.all(Radius.circular(20)),
+          color: Colors.blue),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Icon(
+            Icons.arrow_drop_up,
+            size: 18,
+          ),
+          Icon(
+            Icons.arrow_drop_down,
+            size: 18,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _HomePageState extends State<HomePage> {
+  final List<String> _recvMsg = []; // 接收到的消息
+  double _recvMsgScrollbarAlignmentY = -1; // 范围-1~1
+  double _recvMsgMaxScrollExtent = 0;
+  final _recvMsgListHeight = 200.0;
+
+  String _localIP = '127.0.0.1';  //本机局域网IP
+  RawDatagramSocket _bindSocket = null;
+
+  TextEditingController _sendMsgController =
+  TextEditingController(); //  发送消息文本控制器
+  TextEditingController _IPTxtController =
+  TextEditingController(); //  连接服务器IP文本控制器
+  TextEditingController _portTxtController =
+  TextEditingController(); //  连接服务器端口文本控制器
+
+  @override
+  void initState() {
+    super.initState();
+
+    _IPTxtController.text = _localIP;
+    _portTxtController.text = '23300';
+
+    initIP();  // 尝试获取本机局域网IP
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('TCP_Test'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            createRecvMsgListview(), // 接收消息列表框
+            createIPSettingRect(), // 设置IP、端口区域
+            createSendMsgRect(), // 发送消息区域
+          ],
+        ),
+      ),
+    );
+  }
+
+  // 获取本机局域网IP
+  void initIP() async{
+    try {
+      WifiIpInfo wifiInfo;
+      wifiInfo = await WifiIp.getWifiIp;
+      _localIP = wifiInfo.ip;
+    } on PlatformException {
+      print('Failed to get broadcast IP.');
+    } catch (e) {
+      printLog('尝试获取本机局域网IP异常，e=${e.toString()}');
+    }
+
+    setState(() {
+      _IPTxtController.text = _localIP;
+    });
+  }
+
+  // 接收消息列表框
+  Widget createRecvMsgListview() {
+    return Container(
+      margin: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Colors.black,
+        ),
+        color: Colors.black12,
+      ),
+      height: _recvMsgListHeight,
+      child: Stack(
+        alignment: Alignment.topRight,
+        children: [
+          createMsgListview(), // 消息列表
+          Container(
+            // 滚动条
+            alignment: Alignment(1, _recvMsgScrollbarAlignmentY),
+            padding: EdgeInsets.only(right: 5),
+            child: _ScrollBar(_recvMsgMaxScrollExtent + _recvMsgListHeight,
+                _recvMsgListHeight),
+          )
+        ],
+      ),
+    );
+  }
+  /*****************************************************************************/
+
+  /// IP设置区域
+  // bind、listen
+  void onBtnListen() async{
+    try{
+      _bindSocket = await RawDatagramSocket.bind(_localIP,  int.parse(_portTxtController.text));
+      _bindSocket.listen(onSocketEvent, onError: onSocketError, onDone: onSocketClose);
+    }catch(e){
+      printLog('开始监听出现异常，e=${e.toString()}');
+    }
+  }
+
+  // 关闭socket
+  void onBtnCloseSocket(){
+    _bindSocket.close();
+    _bindSocket = null;
+
+    setState(() {});
+  }
+
+  // socket事件
+  void onSocketEvent(RawSocketEvent e){
+    switch(e){
+      case RawSocketEvent.read:
+        {
+          Datagram dg = _bindSocket.receive();
+          var decoder = Utf8Decoder();
+          String msg = decoder.convert(dg.data); // 将UTF8数据解码
+          printLog('收到来自${dg.address.toString()}:${dg.port}的数据：${dg.data.length}字节数据 内容:$msg');
+        }
+        break;
+      case RawSocketEvent.write:
+        {
+          printLog('RawSocketEvent.write');
+        }
+        break;
+      case RawSocketEvent.readClosed:
+        {
+          printLog('RawSocketEvent.readClosed');
+        }
+        break;
+      case RawSocketEvent.closed:
+        {
+          printLog('RawSocketEvent.closed');
+        }
+        break;
+    }
+  }
+
+  // socket关闭
+  void onSocketClose() {
+    _bindSocket = null;
+    printLog('socket关闭');
+  }
+
+  // socket错误
+  void onSocketError(Object error) {
+    printLog('socket出现错误，error=${error.toString()}');
+  }
+
+  Widget createIPSettingRect() {
+    return Container(
+      margin: EdgeInsets.all(10),
+      child: Wrap(
+        direction: Axis.vertical,
+        spacing: 10,
+        children: [
+          // IP地址
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text('主机IP：'),
+              Container(
+                // 端口输入区域
+                width: 200,
+                height: 50,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.black,
+                  ),
+                  color: Colors.white,
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextField(
+                      maxLines: 1,
+                      controller: _IPTxtController,
+                      decoration: InputDecoration(border: InputBorder.none),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+
+          // 端口
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text('主机端口：'),
+              Container(
+                // 端口输入区域
+                width: 100,
+                height: 50,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.black,
+                  ),
+                  color: Colors.white,
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextField(
+                      maxLines: 1,
+                      controller: _portTxtController,
+                      decoration: InputDecoration(border: InputBorder.none),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+
+          // 按钮
+          Wrap(
+            spacing: 10,
+            children: [
+              _bindSocket == null ? RaisedButton(onPressed: onBtnListen, child: Text('监听')) : RaisedButton(onPressed: onBtnCloseSocket, child: Text('关闭socket')),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+  /********************************************************************/
+
+  /// 发送消息区域，包含发送输入区域和发送按钮
+  // 发送消息
+  void onBtnSendMsg() async {
+    if (_sendMsgController.text.isNotEmpty) {
+      if (_bindSocket != null) {
+        var encoder = Utf8Encoder(); // 创建UTF8转换器，以支持发送中文
+        _bindSocket.send(encoder.convert(_sendMsgController.text),  InternetAddress.tryParse(_IPTxtController.text),  int.tryParse(_portTxtController.text)); // 发送
+      }
+
+      _sendMsgController.text = '';
+      setState(() {});
+    }
+  }
+
+  // 连续发送多条消息
+  void onBtnSendmultipleMsg() {
+    var encoder = Utf8Encoder();
+    var data = encoder.convert(_sendMsgController.text);
+    for (int i = 0; i < 100; ++i) {
+      _bindSocket.send(data,  InternetAddress.tryParse(_IPTxtController.text),  int.tryParse(_portTxtController.text)); // 发送
+    }
+  }
+
+  Widget createSendMsgRect() {
+    return Container(
+      margin: EdgeInsets.all(10),
+      child: Column(
+        children: [
+          Container(
+            // 输入区域
+            height: 40,
+            alignment: Alignment.centerLeft,
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.black,
+              ),
+              color: Colors.white,
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                maxLines: 1,
+                controller: _sendMsgController,
+                autofocus: true,
+                decoration: InputDecoration(border: InputBorder.none),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Wrap(
+            spacing: 10,
+            children: [
+              RaisedButton(onPressed: onBtnSendMsg, child: Text('发送消息')),
+              RaisedButton(onPressed: onBtnSendmultipleMsg, child: Text('发送多条消息')),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  // 展示接收到的消息Listview
+  bool _handleScrollNotification(ScrollNotification notification) {
+    final ScrollMetrics metrics = notification.metrics;
+    // printLog('滚动组件最大滚动距离:${metrics.maxScrollExtent}');
+    // printLog('当前滚动位置:${metrics.pixels}');
+
+    _recvMsgMaxScrollExtent = metrics.maxScrollExtent == double.infinity
+        ? 0
+        : metrics.maxScrollExtent;
+    _recvMsgScrollbarAlignmentY = metrics.maxScrollExtent == 0
+        ? -1
+        : -1 + (metrics.pixels / metrics.maxScrollExtent) * 2;
+    // printLog('_alignmentY:$_alignmentY');
+
+    setState(() {});
+    return true;
+  }
+
+  Widget createMsgListview() {
+    return NotificationListener<ScrollNotification>(
+      onNotification: _handleScrollNotification,
+      child: ListView.builder(
+        itemBuilder: (context, index) {
+          return Text(_recvMsg[index]);
+        },
+        itemCount: _recvMsg.length,
+      ),
+    );
+  }
+  /**********************************************************************/
+
+  // 打印日志
+  void printLog(String log) {
+    print(log);
+    _recvMsg.add(log);
+    setState(() {});
+  }
+}
