09d5a8584bc4616fde991f7f265244fbac7e3eab
wangyan <tilongzs@qq.com>
2022-08-29 09:41:14 +0800

    例行提交

--- socket_udp_test/lib/main.dart
+++ socket_udp_test/lib/main.dart
@@ -1,9 +1,8 @@
 import 'dart:convert';
 import 'dart:io';
-import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:wifi_ip/wifi_ip.dart';
+import 'package:network_info_plus/network_info_plus.dart';
 
 void main() {
   runApp(MyApp());
@@ -29,71 +28,21 @@ class HomePage extends StatefulWidget {
   }
 }
 
-// 自定义滚动条
-class _ScrollBar extends StatelessWidget {
-  double _viewHeight = 1;
-  double _parentHeight = 1;
-
-  _ScrollBar(double viewHeight, double parentHeight) {
-    _viewHeight = viewHeight;
-    _parentHeight = parentHeight;
-  }
-
-  double GenerateHeight() {
-    if (_viewHeight == 0) {
-      return 0;
-    } else {
-      double height = _parentHeight * _parentHeight / _viewHeight;
-      return height < 50 ? 50 : height;
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 18,
-      height: GenerateHeight(),
-      decoration: BoxDecoration(
-          shape: BoxShape.rectangle,
-          borderRadius: BorderRadius.all(Radius.circular(20)),
-          color: Colors.blue),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Icon(
-            Icons.arrow_drop_up,
-            size: 18,
-          ),
-          Icon(
-            Icons.arrow_drop_down,
-            size: 18,
-          ),
-        ],
-      ),
-    );
-  }
-}
-
 class _HomePageState extends State<HomePage> {
   final List<String> _recvMsg = []; // 接收到的消息
-  double _recvMsgScrollbarAlignmentY = -1; // 范围-1~1
-  double _recvMsgMaxScrollExtent = 0;
   final _recvMsgListHeight = 200.0;
 
   String _localIP = '127.0.0.1';  //本机局域网IP
-  RawDatagramSocket _bindSocket;
+  RawDatagramSocket? _bindSocket;
 
-  TextEditingController _sendMsgController =
-  TextEditingController(); //  发送消息文本控制器
-  TextEditingController _ipTxtController =
-  TextEditingController(); //  连接服务器IP文本控制器
-  TextEditingController _portTxtController =
-  TextEditingController(); //  连接服务器端口文本控制器
+  TextEditingController _recvMsgController = TextEditingController(); // 接收消息文本控制器
+  TextEditingController _sendMsgController = TextEditingController(); //  发送消息文本控制器
+  TextEditingController _ipTxtController = TextEditingController(); //  连接服务器IP文本控制器
+  TextEditingController _portTxtController = TextEditingController(); //  连接服务器端口文本控制器
 
   @override
   void initState() {
     super.initState();
-
     _ipTxtController.text = _localIP;
     _portTxtController.text = '23300';
 
@@ -101,17 +50,27 @@ class _HomePageState extends State<HomePage> {
   }
 
   @override
+  void dispose() {
+    _recvMsgController.dispose();
+    _sendMsgController.dispose();
+    _ipTxtController.dispose();
+    _portTxtController.dispose();
+
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('TCP_Test'),
+        title: Text('UDP_Test'),
       ),
       body: Center(
         child: Column(
           children: [
-            createRecvMsgListview(), // 接收消息列表框
-            createIPSettingRect(), // 设置IP、端口区域
-            createSendMsgRect(), // 发送消息区域
+            recvMsgListview(), // 接收消息列表框
+            IPSettingRect(), // 设置IP、端口区域
+            sendMsgRect(), // 发送消息区域
           ],
         ),
       ),
@@ -121,9 +80,9 @@ class _HomePageState extends State<HomePage> {
   // 获取本机局域网IP
   void initIP() async{
     try {
-      WifiIpInfo wifiInfo;
-      wifiInfo = await WifiIp.getWifiIp;
-      _localIP = wifiInfo.ip;
+      final wifiInfo = NetworkInfo();
+      var wifiIP = await wifiInfo.getWifiIP();
+      _localIP = wifiIP == null ? "" : wifiIP;
     } on PlatformException {
       print('Failed to get broadcast IP.');
     } catch (e) {
@@ -136,28 +95,24 @@ class _HomePageState extends State<HomePage> {
   }
 
   // 接收消息列表框
-  Widget createRecvMsgListview() {
+  Widget recvMsgListview() {
     return Container(
       margin: EdgeInsets.all(10),
       decoration: BoxDecoration(
-        border: Border.all(
-          color: Colors.black,
-        ),
         color: Colors.black12,
       ),
       height: _recvMsgListHeight,
-      child: Stack(
-        alignment: Alignment.topRight,
-        children: [
-          createMsgListview(), // 消息列表
-          Container(
-            // 滚动条
-            alignment: Alignment(1, _recvMsgScrollbarAlignmentY),
-            padding: EdgeInsets.only(right: 5),
-            child: _ScrollBar(_recvMsgMaxScrollExtent + _recvMsgListHeight,
-                _recvMsgListHeight),
-          )
-        ],
+      child: TextField(
+        readOnly: true,
+        controller: _recvMsgController,
+        expands: true, // 填充父窗口
+        maxLines: null,
+        keyboardType: TextInputType.multiline,
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          contentPadding: EdgeInsets.all(10)
+        ),
+        textAlignVertical: TextAlignVertical.top,
       ),
     );
   }
@@ -168,7 +123,7 @@ class _HomePageState extends State<HomePage> {
   void onBtnListen() async{
     try{
       _bindSocket = await RawDatagramSocket.bind(_localIP,  int.parse(_portTxtController.text));
-      _bindSocket.listen(onSocketData, onError: onSocketError, onDone: onSocketDone);
+      _bindSocket?.listen(onSocketData, onError: onSocketError, onDone: onSocketDone);
     }catch(e){
       printLog('开始监听出现异常，e=${e.toString()}');
     }
@@ -176,7 +131,7 @@ class _HomePageState extends State<HomePage> {
 
   // 关闭socket
   void onBtnCloseSocket(){
-    _bindSocket.close();
+    _bindSocket?.close();
     _bindSocket = null;
 
     setState(() {});
@@ -185,25 +140,26 @@ class _HomePageState extends State<HomePage> {
   // socket事件
   void onSocketData(RawSocketEvent e){
     switch(e){
-      case RawSocketEvent.read:
-        {
-          Datagram dg = _bindSocket.receive();
-          String msg = utf8.decode(dg.data); // 将UTF8数据解码
-          printLog('收到来自${dg.address.toString()}:${dg.port}的数据：${dg.data.lengthInBytes}字节数据 内容:$msg');
-        }
+      case RawSocketEvent.read:{
+          if(_bindSocket != null){
+            Datagram? dg = _bindSocket!.receive();
+            if(dg != null){
+              String msg = utf8.decode(dg!.data); // 将UTF8数据解码
+              _recvMsgController.text += ("\r\n" + msg);
+              printLog('收到来自${dg.address.toString()}:${dg.port}的数据：${dg.data.lengthInBytes}字节数据 内容:$msg');
+            }
+          }
+         }
         break;
-      case RawSocketEvent.write:
-        {
+      case RawSocketEvent.write: {
           printLog('RawSocketEvent.write');
         }
         break;
-      case RawSocketEvent.readClosed:
-        {
+      case RawSocketEvent.readClosed:{
           printLog('RawSocketEvent.readClosed');
         }
         break;
-      case RawSocketEvent.closed:
-        {
+      case RawSocketEvent.closed: {
           printLog('RawSocketEvent.closed');
         }
         break;
@@ -221,7 +177,7 @@ class _HomePageState extends State<HomePage> {
     printLog('socket出现错误，error=${error.toString()}');
   }
 
-  Widget createIPSettingRect() {
+  Widget IPSettingRect() {
     return Container(
       margin: EdgeInsets.all(10),
       child: Wrap(
@@ -234,24 +190,17 @@ class _HomePageState extends State<HomePage> {
             children: [
               Text('主机IP：'),
               Container(
-                // 端口输入区域
                 width: 200,
-                height: 50,
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.black,
+                height: 40,
+                alignment: Alignment.centerLeft,
+                child: TextField(
+                  maxLines: 1,
+                  controller: _ipTxtController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    contentPadding: EdgeInsets.only(left: 5),
                   ),
-                  color: Colors.white,
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    TextField(
-                      maxLines: 1,
-                      controller: _ipTxtController,
-                      decoration: InputDecoration(border: InputBorder.none),
-                    ),
-                  ],
+                  textAlignVertical: TextAlignVertical.center,
                 ),
               ),
             ],
@@ -262,25 +211,17 @@ class _HomePageState extends State<HomePage> {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Text('主机端口：'),
-              Container(
-                // 端口输入区域
+              SizedBox(
                 width: 100,
-                height: 50,
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.black,
+                height: 40,
+                child: TextField(
+                  maxLines: 1,
+                  controller: _portTxtController,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      contentPadding: EdgeInsets.only(left: 5),
                   ),
-                  color: Colors.white,
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    TextField(
-                      maxLines: 1,
-                      controller: _portTxtController,
-                      decoration: InputDecoration(border: InputBorder.none),
-                    ),
-                  ],
+                  textAlignVertical: TextAlignVertical.center,
                 ),
               ),
             ],
@@ -290,7 +231,7 @@ class _HomePageState extends State<HomePage> {
           Wrap(
             spacing: 10,
             children: [
-              _bindSocket == null ? RaisedButton(onPressed: onBtnListen, child: Text('监听')) : RaisedButton(onPressed: onBtnCloseSocket, child: Text('关闭socket')),
+              _bindSocket == null ? ElevatedButton(onPressed: onBtnListen, child: Text('监听')) : ElevatedButton(onPressed: onBtnCloseSocket, child: Text('关闭socket')),
             ],
           )
         ],
@@ -304,7 +245,11 @@ class _HomePageState extends State<HomePage> {
   void onBtnSendMsg() async {
     if (_sendMsgController.text.isNotEmpty) {
       if (_bindSocket != null) {
-        _bindSocket.send(utf8.encode(_sendMsgController.text),  InternetAddress.tryParse(_ipTxtController.text),  int.tryParse(_portTxtController.text)); // 发送
+        var remoteAddr = InternetAddress.tryParse(_ipTxtController.text);
+        remoteAddr ??= InternetAddress("127.0.0.1");
+        var remotePort = int.tryParse(_portTxtController.text);
+        remotePort ??= 23300;
+        _bindSocket!.send(utf8.encode(_sendMsgController.text),  remoteAddr, remotePort); // 发送
       }
 
       _sendMsgController.text = '';
@@ -315,34 +260,36 @@ class _HomePageState extends State<HomePage> {
   // 连续发送多条消息
   void onBtnSendmultipleMsg() {
     var data = utf8.encode(_sendMsgController.text);
-    for (int i = 0; i < 100; ++i) {
-      _bindSocket.send(data,  InternetAddress.tryParse(_ipTxtController.text),  int.tryParse(_portTxtController.text)); // 发送
+    var remoteAddr = InternetAddress.tryParse(_ipTxtController.text);
+    remoteAddr ??= InternetAddress("127.0.0.1");
+    var remotePort = int.tryParse(_portTxtController.text);
+    remotePort ??= 23300;
+    if (_bindSocket != null) {
+      for (int i = 0; i < 100; ++i) {
+        _bindSocket!.send(data, remoteAddr, remotePort); // 发送
+      }
     }
   }
 
-  Widget createSendMsgRect() {
+  Widget sendMsgRect() {
     return Container(
       margin: EdgeInsets.all(10),
       child: Column(
         children: [
           Container(
-            // 输入区域
-            height: 40,
+            // 多行输入区域
+            height: 120,
             alignment: Alignment.centerLeft,
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: Colors.black,
+            child: TextField(
+              controller: _sendMsgController,
+              autofocus: true,
+              expands: true, // 填充父窗口
+              maxLines: null,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                contentPadding: EdgeInsets.all(10)
               ),
-              color: Colors.white,
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                maxLines: 1,
-                controller: _sendMsgController,
-                autofocus: true,
-                decoration: InputDecoration(border: InputBorder.none),
-              ),
+              textAlignVertical: TextAlignVertical.top,
             ),
           ),
           SizedBox(
@@ -351,8 +298,8 @@ class _HomePageState extends State<HomePage> {
           Wrap(
             spacing: 10,
             children: [
-              RaisedButton(onPressed: onBtnSendMsg, child: Text('发送消息')),
-              RaisedButton(onPressed: onBtnSendmultipleMsg, child: Text('发送多条消息')),
+              ElevatedButton(onPressed: onBtnSendMsg, child: Text('发送消息')),
+              ElevatedButton(onPressed: onBtnSendmultipleMsg, child: Text('发送多条消息')),
             ],
           ),
         ],
@@ -360,27 +307,8 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  // 展示接收到的消息Listview
-  bool _handleScrollNotification(ScrollNotification notification) {
-    final ScrollMetrics metrics = notification.metrics;
-    // printLog('滚动组件最大滚动距离:${metrics.maxScrollExtent}');
-    // printLog('当前滚动位置:${metrics.pixels}');
-
-    _recvMsgMaxScrollExtent = metrics.maxScrollExtent == double.infinity
-        ? 0
-        : metrics.maxScrollExtent;
-    _recvMsgScrollbarAlignmentY = metrics.maxScrollExtent == 0
-        ? -1
-        : -1 + (metrics.pixels / metrics.maxScrollExtent) * 2;
-    // printLog('_alignmentY:$_alignmentY');
-
-    setState(() {});
-    return true;
-  }
-
   Widget createMsgListview() {
     return NotificationListener<ScrollNotification>(
-      onNotification: _handleScrollNotification,
       child: ListView.builder(
         itemBuilder: (context, index) {
           return Text(_recvMsg[index]);
