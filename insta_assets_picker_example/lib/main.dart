83f54d0a27054fb3059855a6a9dc0e673762d46f
tilongzs <tilongzs@qq.com>
2024-07-19 15:37:07 +0800

    修改insta_assets_picker插件示例

--- insta_assets_picker_example/lib/main.dart
+++ insta_assets_picker_example/lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'insta_assets_picker插件示例'),
     );
   }
 }
@@ -32,6 +32,38 @@ class MyHomePage extends StatefulWidget {
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
+}
+
+Future<Uint8List?> readAlbumPhoto(BuildContext context) async {
+  final completer = Completer<Uint8List?>();
+
+  final theme = InstaAssetPicker.themeData(Theme.of(context).primaryColor);
+  await InstaAssetPicker.pickAssets(
+    context,
+    title: '选择图像',
+    maxAssets: 1,
+    onCompleted: (Stream<InstaAssetsExportDetails> stream) {
+      stream.listen((data) async {
+        if (data.progress == 1.0) {
+          print('stream data progress:${data.progress} aspectRatio:${data.aspectRatio}');
+          final imageBytes = await data.croppedFiles.first.readAsBytes();
+          completer.complete(imageBytes); // 完成 Completer
+        }
+      });
+    },
+    onPermissionDenied: (context, delegateDescription) {
+      print('访问相册被拒绝');
+      completer.complete(null); // 处理权限被拒绝的情况
+    },
+    cropDelegate: InstaAssetCropDelegate(preferredSize: 1080, cropRatios: [3/4]),
+    pickerTheme: theme.copyWith(
+      // ... 省略其他配置
+    ),
+    closeOnComplete: true,
+    textDelegate: AssetPickerTextDelegate(),
+  );
+
+  return completer.future; // 返回 Completer 的 future
 }
 
 class _MyHomePageState extends State<MyHomePage> {
@@ -51,16 +83,18 @@ class _MyHomePageState extends State<MyHomePage> {
           borderRadius: BorderRadius.circular(20),
           child: _imageBytes != null
               ? Center(
-            child: Image.memory(
-              _imageBytes!,
-              width: 150, // 设置图像的宽度
-              height: 200, // 设置图像的高度
-              fit: BoxFit.cover, // 设置适应模式
-            ),
-          )
-              : Icon(Icons.photo_album_outlined, size: 150,),
-        )
-    );
+                  child: Image.memory(
+                    _imageBytes!,
+                    width: 150, // 设置图像的宽度
+                    height: 200, // 设置图像的高度
+                    fit: BoxFit.cover, // 设置适应模式
+                  ),
+                )
+              : Icon(
+                  Icons.photo_album_outlined,
+                  size: 150,
+                ),
+        ));
   }
 
   @override
@@ -87,57 +121,14 @@ class _MyHomePageState extends State<MyHomePage> {
                   //   print('AssetPicker.pickAssets异常 $e');
                   // }
 
-                  final theme = InstaAssetPicker.themeData(Theme.of(context).primaryColor);
-                  await InstaAssetPicker.pickAssets(
-                    context,
-                    title: '选择图像',
-                    maxAssets: 1,
-                    onCompleted: (Stream<InstaAssetsExportDetails> stream) {
-                      stream.listen((data) async {
-                        if(data.progress == 1.0){
-                          print('stream data progress:${data.progress} aspectRatio:${data.aspectRatio}');
-                          _imageBytes = await data.croppedFiles.first.readAsBytes();
-                          setState(() {
-                          });
-                        }
-                      });
-                    },
-                    onPermissionDenied: (context, delegateDescription) {
-                      print('访问相册被拒绝');
-                    },
-                    cropDelegate: InstaAssetCropDelegate(
-                        preferredSize: 1080,
-                        cropRatios: [
-                          3/4
-                        ]),
-                    pickerTheme: theme.copyWith(
-                      canvasColor: Colors.black, // body background color
-                      splashColor: Colors.grey, // ontap splash color
-                      appBarTheme: theme.appBarTheme.copyWith(
-                        backgroundColor: Colors.black, // app bar background color
-                        titleTextStyle: Theme.of(context)
-                            .appBarTheme
-                            .titleTextStyle
-                            ?.copyWith(color: Colors.white), // change app bar title text style to be like app theme
-                      ),
-                      // edit `confirm` button style
-                      textButtonTheme: TextButtonThemeData(
-                        style: TextButton.styleFrom(
-                          foregroundColor: Colors.blue,
-                          disabledForegroundColor: Colors.grey,
-                        ),
-                      ),
-                    ),
-                    closeOnComplete: true,
-                    textDelegate: AssetPickerTextDelegate(), // EnglishAssetPickerTextDelegate(),
-                  );
+                  _imageBytes = await readAlbumPhoto(context);
+                  setState(() { });
                 },
               ),
-
               avatar(),
             ],
           ),
         ) // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        );
   }
 }
